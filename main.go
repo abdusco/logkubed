1e4ede5b915bc1423408d7f607813e6c29deccd3
Abdussamet Kocak <abdus@abdus.dev>
2022-01-23 18:12:00 +0300

    fix: Make app configurable via environment variables

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/pkg/errors"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"log"
 	"os"
@@ -10,27 +11,53 @@ import (
 )
 
 func main() {
-	kubeconfigPath := "/Users/abdus/Desktop/kubeconfig-kube.yml"
-	clientset, err := NewKubernetesClient(kubeconfigPath)
+	var clientset *kubernetes.Clientset
+	var err error
+	kubeconfigPath := os.Getenv("KUBECONFIG_PATH")
+	if kubeconfigPath == "" {
+		log.Println("using in-cluster config")
+		clientset, err = NewKubernetesInClusterClient()
+	} else {
+		log.Println("using kubeconfig")
+		clientset, err = NewKubernetesLocalClient(kubeconfigPath)
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
-	streamer := NewLogStreamer(clientset)
-	broker := NewLogBroker(streamer)
+
 	portEnv := os.Getenv("PORT")
 	if portEnv == "" {
 		portEnv = "8080"
 	}
-	port, _ := strconv.Atoi(portEnv)
+	port, err := strconv.Atoi(portEnv)
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "port must be numeric"))
+	}
+
+	streamer := NewLogStreamer(clientset)
+	broker := NewLogBroker(streamer)
 
 	app := NewApp(port, broker)
 	log.Fatalln(app.Serve())
 }
 
-func NewKubernetesClient(kubeconfigPath string) (*kubernetes.Clientset, error) {
+func NewKubernetesLocalClient(kubeconfigPath string) (*kubernetes.Clientset, error) {
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
 	if err != nil {
 		return nil, errors.Wrap(err, "read kubeconfig")
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, errors.Wrap(err, "create kubernetes client")
+	}
+	return clientset, nil
+}
+
+func NewKubernetesInClusterClient() (*kubernetes.Clientset, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, errors.Wrap(err, "read in-cluster config")
 	}
 
 	clientset, err := kubernetes.NewForConfig(config)
